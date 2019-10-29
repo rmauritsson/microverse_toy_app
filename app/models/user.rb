dd2962f1b6e3f8281f4deacde0b16cd03d8c0a3e
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-10-29 14:45:45 -0300

    rubocop errors fixed :)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :microposts
   validates FILL_IN, presence: true # Replace FILL_IN with the right code.
   validates FILL_IN, presence: true # Replace FILL_IN with the right code
+  has_many :microposts, dependent: :destroy
 end
